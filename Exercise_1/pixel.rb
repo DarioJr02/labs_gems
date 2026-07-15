9c3c09fd730c2193e2775bf10d4a548f560a8a65
DarioJr02 <dariocmia@gmail.com>
2026-07-15 14:50:34 +0200

    Sorbet<b>: Rubocop, Sorbet, Tapioca setup

--- Exercise_1/pixel.rb
+++ Exercise_1/pixel.rb
@@ -1,7 +1,55 @@
-# typed: strict
+# typed: true
 # frozen_string_literal: true
+
+require 'sorbet-runtime'
 
 # The goal of this file is to learn and practice about ->
 # - Sorbet static checker.
 
-puts('Testing')
+# 1. What does mean magic comments in this context?
+
+class Pixel
+  attr_reader :z
+
+  attr_accessor(:x, :y, :z)
+
+  extend T::Sig
+
+  sig { params(x: Float, y: Float, z: Float).void }
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  sig { returns(String) }
+  def print_data
+    return "x=#{@x} | y=#{@y}, | z=#{@z}"
+  end
+
+  sig { returns(Float) }
+  def sqr_position
+    @x *= 2
+    @y *= 2
+    @z *= 2
+  end
+
+  sig { params(name: String).returns(String) }
+  def print_name(name)
+    return name
+  end
+end
+
+p1 = Pixel.new(1.3, 2.2, 'a'.ord.to_f)
+
+p(p1)
+
+puts(p1.print_data)
+p1.sqr_position
+puts(p1.print_data)
+
+arr = p1.sqr_position
+puts(arr)
+
+name = p1.print_name("Dario")
+puts(name)
--- Gemfile
+++ Gemfile
@@ -4,9 +4,10 @@ source 'https://rubygems.org'
 
 # gem "rails"
 
-gem 'rubocop', '~> 1.88'
+gem 'rubocop'
 gem 'sorbet'
-# gem 'sorbet-runtime'
-gem 'sorbet-static-and-runtime'
+gem 'sorbet-runtime'
 
-gem "tapioca", "~> 0.19.2"
+gem 'tapioca', '~> 0.19.2'
+
+gem 'ruby-lsp', '~> 0.26.10'
