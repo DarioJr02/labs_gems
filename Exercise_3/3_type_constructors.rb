48085fd54dcc5d344760801195290248de634c42
DarioJr02 <dariocmia@gmail.com>
2026-07-16 11:10:12 +0200

    Sorbet<b>: Ex3: Accesors

--- Exercise_3/3_type_constructors.rb
+++ Exercise_3/3_type_constructors.rb
@@ -9,8 +9,15 @@ require 'sorbet-runtime'
 # Pixel class
 class Pixel
   extend T::Sig
-  # 1. Constructor.
-  def Initialize
-    
+
+  # 1. Accesors.
+  attr_accessor(:x, :y, :z)
+
+  # 2. Constructor.
+  sig { params(x: Float, y: Float, z: Float).void }
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
   end
 end
