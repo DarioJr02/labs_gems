c729b145201069001da765b15fbd0cac8c2bf5e3
DarioJr02 <dariocmia@gmail.com>
2026-07-16 11:06:13 +0200

    Sorbet<b>: Ex3: Type constructors.

---
+++ Exercise_3/3_type_constructors.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+# typed: true
+
+# The goal of this file is to learn about ->
+# - Type constructors.
+
+require 'sorbet-runtime'
+
+# Pixel class
+class Pixel
+  extend T::Sig
+  # 1. Constructor.
+  def Initialize
+    
+  end
+end
